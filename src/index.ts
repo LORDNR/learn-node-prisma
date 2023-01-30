61430390f9e0cb5cd23129fc6c8593c93bea1c43
LORDNR <ratchanons.work@gmail.com>
2023-01-30 10:40:51 +0700

    🎉 Begin a project.

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import express from 'express'
+import type { Request, Response } from 'express'
+import cors from 'cors'
+
+import config from './config'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Hello World!',
+    })
+})
+
+app.listen(config.environments.port, () =>
+    console.log(`http://localhost:${config.environments.port}`)
+)
